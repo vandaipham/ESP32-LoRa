21fc5d85c31ddd35fdd6247ddb409310405b054f
Van Dai Pham <vdpham.info@gmail.com>
2022-12-06 19:55:16 +0700

    Add WiFi and Firebase configuration

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,6 +1,18 @@
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
+#include <FirebaseESP32.h>
+#include <WiFi.h>
 
+
+// WiFi configuration
+#define WIFI_SSID "IphoneXr"                                          // input your home or public wifi name
+#define WIFI_PASSWORD "11111111"
+
+// Firebase configuration
+#define FIREBASE_HOST "doan-3f0dd-default-rtdb.firebaseio.com"      // the project name address from firebase id
+#define FIREBASE_AUTH "qzKiNHBVt3BLsLWKJGZC9Hpu2VN7dlvWytY5AjXh"    // the secret key generated from firebase
+
+FirebaseData dulieufirebase;
 
 StaticJsonDocument<200> doc;
 
@@ -20,6 +32,38 @@ int pos = 0;
 void setup()
 {
   Serial.begin(9600);
+
+  // Begin WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+     delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  
+  // Begin Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi("true");
+
+  setData("Mayphunsuong", "false");
+  setData("Maysuoi", "false");
+  setData("Mayquat", "false");
+  setData("Loa", "false");
+  // setData("Keo_man_che_len","false");
+  // setData("Keo_man_che_xuong","false");  
+  setData("AutoMode", "false");
+  setData("GH_NhietDo_LOW", "28");
+  setData("GH_NhietDo_High", "30");
+  setData("GH_DoAm_High", "80");
+  setData("GH_DoAm_LOW", "60");
+  setData("GH_AS_High", "50"); 
+  setData("GH_AS_Low", "30"); 
+  setData("Giobatloa", "false"); 
+  setData("Giotatloa", "false");
 
   // Define pin modes for TX and RX
   pinMode(rxPin, INPUT);
@@ -58,11 +102,27 @@ void loop()
       Serial.print("h = "); Serial.println(h);
       Serial.print("lux = "); Serial.println(lux);
 
+      // Send received data to Firebase
+      Firebase.setInt(dulieufirebase, "Nhiet_Do", t);
+      Firebase.setInt(dulieufirebase, "Do_Am", h);
+      Firebase.setInt(dulieufirebase, "CuongDoAnhSang", lux);
+
       // khoi tao lai char array
       pos = 0;
     } else {
       dataRcvd[pos] = inByte;
       pos++;
     }
+  }
+}
+
+void setData(String url, String data1){
+  if(Firebase.setString(dulieufirebase, url, data1)){
+    //Success
+    Serial.println("Set string data success");
+  } else {
+    //Failed?, get the error reason from firebaseData
+    Serial.print("Error in setString, ");
+    Serial.println(dulieufirebase.errorReason());
   }
 }
